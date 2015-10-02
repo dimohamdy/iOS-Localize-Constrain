ffbf9dc9b4e345656c6410c0d8751d0694b095d7
hamdy <dimo.hamdy@gmail.com>
2015-10-03 01:36:15 +0200

    prevent warning of constraint

--- LocalizeConstrain/AppDelegate.m
+++ LocalizeConstrain/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[NSUserDefaults standardUserDefaults] setValue:@(NO) forKey:@"_UIConstraintBasedLayoutLogUnsatisfiable"];
+
     return YES;
 }
 
