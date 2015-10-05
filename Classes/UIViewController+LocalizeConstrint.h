fc4cdb45d8c510fc833d898b3184249018ae23f2
hamdy <dimo.hamdy@gmail.com>
2015-10-06 00:59:30 +0200

    push classes Folder
    update pod

---
+++ Classes/UIViewController+LocalizeConstrint.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+LocalizeConstrint.h
+//  LocalizeConstraint
+//
+//  Created by Hamdy on 10/4/15.
+//  Copyright (c) 2015 Hamdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (LocalizeConstrint)
+-(void)changeViewRTL:(UIView*)tempView;
+@end
